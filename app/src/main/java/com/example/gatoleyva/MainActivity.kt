17dc55b7ccd63dadb232bdc1e1dbdb7dc4d519e6
Luis Leyva <leyva.garcia.luis.gerardo@gmail.com>
2022-08-31 19:21:15 -0500

    Diseño terminado, implementacion de DataBinding

--- app/src/main/java/com/example/gatoleyva/MainActivity.kt
+++ app/src/main/java/com/example/gatoleyva/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gatoleyva
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gatoleyva.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // DataBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
